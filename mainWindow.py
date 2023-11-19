bc9677419716b2c830961940b4a804c88c1078c4
Mo Khan <60451707+Mo-K-K@users.noreply.github.com>
2023-11-19 14:22:43 +0000

    Added Label to display what file is selected

--- mainWindow.py
+++ mainWindow.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget, QDialog, QApplication, QFileDialog
+from PyQt5.QtWidgets import QHBoxLayout, QLabel, QApplication, QMainWindow, QVBoxLayout, QPushButton, QWidget, QDialog, QApplication, QFileDialog
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -9,19 +9,29 @@ class MainWindow(QMainWindow):
         
     def initUI(self):
         #initialise UI
-        self.app = app
         self.setWindowTitle("Main Window")
+        self.setGeometry(200, 200, 800, 600)
+        self.fname = None
+        
+        main_widget = QWidget(self)
+        self.setCentralWidget(main_widget)
+        layout = QVBoxLayout(main_widget)
         
         #Create menu bar with name "File" and sub menu "Add File" 
-        menu_bar = self.menuBar()
-        file_menu = menu_bar.addMenu("&File")
-        Browse_action = file_menu.addAction("Add File")
-        Browse_action.triggered.connect(self.Browseapp)
+        self.menu_bar = self.menuBar()
+        self.file_menu = self.menu_bar.addMenu("&File")
+        self.Browse_action = self.file_menu.addAction("Add File")
+        self.Browse_action.triggered.connect(self.Browseapp)
+        
+        #Label to show chosen file
+        self.Label = QLabel("File Name: ")
+        layout.addWidget(self.Label)
+        
         
     def Browseapp(self):
         #Opens file browser and saves chosen files name
         fname  = QFileDialog.getOpenFileName(self, 'Open file')
-        print("name of file", fname[0])
+        self.Label.setText("File Name: " + str(fname[0]))
         
         
 if __name__ == '__main__':
