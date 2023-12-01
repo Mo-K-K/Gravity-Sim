390f646fc58f8760f10e9bd0eb3368628a762dc9
Mo Khan <60451707+Mo-K-K@users.noreply.github.com>
2023-12-01 15:52:52 +0000

    renamed calcualte.py to PlotOfParticles.py

--- Calculate.py
+++
@@ -1,6 +0,0 @@
-
-
-class calculate:
-    def __init__(self, pos=None, vel=None, acc=None):
-        #2D array of pos, vel and acc which will give acceleration of first form second body
-        None
